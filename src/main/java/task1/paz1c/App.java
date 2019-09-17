c741922dc57fab8f798640fb9795e2c514ead951
student <student@158.197.35.91>
2019-09-17 10:35:33 +0200

    Message from Bill G.

--- src/main/java/task1/paz1c/App.java
+++ src/main/java/task1/paz1c/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        //System.out.println( "Hello Germany!" );
+        System.out.println( "Hello Germany!" );
+        
+        //Excellent work
+        //Bill Gates
     }
 }
